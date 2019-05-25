5bbf32d7d0bcf4464b039be80167ab6abc425a12
Lino Espinoza <admin@linoespinoza.com>
2019-05-24 22:02:00 -0500

    first commit

---
+++ song.js
@@ -0,0 +1,31 @@
+/*
+This is a javascript file called song with many classic atributes.  
+*/
+
+// Genre of the song
+var genre = "jazz"
+
+// Name of the artist
+var artist = "Jose Perez"
+
+// Duration of the song in seconds
+var durationInSeconds = "328"
+
+// Is this a Rock and Roll song?
+var isRockAndRoll = false
+
+// Name of the album
+var albumName = "The greatest album ever"
+
+// Object with reference styles of the song
+var referencesStyles = {
+    "artist": "Joshua Javascript",
+    "genre": "Blues"
+}
+
+console.log(artist);
+console.log(genre);
+console.log(durationInSeconds);
+console.log(isRockAndRoll);
+console.log(albumName);
+console.log(referencesStyles);
